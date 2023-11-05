aea410256283c4559b8946db951e50810f6c98f8
Russ <Pokerchips90@gmail.com>
2023-11-05 17:41:19 -0500

    Working on piece generation

--- Assets/Shape/Scripts/Shape.cs
+++ Assets/Shape/Scripts/Shape.cs
@@ -8,15 +8,44 @@ public class Shape : MonoBehaviour
     private Vector2[] nodePositions;
     
     
-    public Shape(Vector2 nodePositions)
+    public Shape(Vector2[] nodePositions)
     {
         this.nodePositions = nodePositions;
+        CreateMesh();
     }
 
-    private CreatMesh()
+    private void CreateMesh()
     {
         Vector3[] vertices = new Vector3[4];
-        Vector2[] uv = new Vector2[0];
-        int[] triangles = new int[0];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
+        
+        vertices[0] = new Vector3(0, 0);
+        vertices[1] = new Vector3(0, 100);
+        vertices[2] = new Vector3(100, 100);
+        vertices[3] = new Vector3(100, 0);
+        
+        uv[0] = new Vector2(0, 0);
+        uv[1] = new Vector2(0, 1);
+        uv[2] = new Vector2(1, 1);
+        uv[3] = new Vector2(1, 0);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 2;
+        triangles[4] = 3;
+        triangles[5] = 0;
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+        mesh.name = "Piece";
+        
+        GameObject gameObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
+        gameObject.transform.localScale = new Vector3(1, 1, 1);
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.name = "Piece";
     }
 }
--- Assets/Shape/Scripts/ShapeGenerator.cs
+++ Assets/Shape/Scripts/ShapeGenerator.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ShapeGenerator : MonoBehaviour
 {
+    public Vector2[] nodePositions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shape shape = new Shape(nodePositions);
     }
 
     // Update is called once per frame
