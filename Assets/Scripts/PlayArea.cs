6f969a437c40fd6599a0d9f4f25c36840d73c255
Kyle Ericson <kyle.ericson2@gmail.com>
2023-11-06 17:25:57 -0500

    working on the algorithm for cutting the grid into pieces

--- Assets/Scripts/PlayArea.cs
+++ Assets/Scripts/PlayArea.cs
@@ -20,10 +20,6 @@ public class PlayArea : MonoBehaviour
         
     }
 
-    public Grid2D GetGrid()
-    {
-        return grid;
-    }
 
     public void BuildPlayArea(int gridWidth, int gridHeight)
     {
@@ -36,6 +32,41 @@ public class PlayArea : MonoBehaviour
             cells[i].transform.position = grid.Index2World(i);
             cells[i].transform.SetParent(this.gameObject.transform);
         }
+    }
+
+    public void ColorTheCells(Vector2[] pos, Color c)
+    {
+        int pI = grid.Cart2Index(pos[0]);
+        Transform parent = cells[pI].transform;
+
+        foreach(Vector2 p in pos)
+        {
+            int i = grid.Cart2Index(p);
+            SpriteRenderer sr = cells[i].GetComponent<SpriteRenderer>();
+            sr.color = c;
+            if(p != pos[0])
+                cells[i].transform.SetParent(parent);
+        }
+    }
+
+    public int Size()
+    {
+        return grid.Size;
+    }
+
+    public Grid2D GetGrid()
+    {
+        return grid;
+    }
+
+    public Vector2 Index2Cart(int n)
+    {
+        return grid.Index2Cart(n);
+    }
+
+    public int Cart2Index(Vector2 p)
+    {
+        return grid.Cart2Index(p);
     }
 
 }
