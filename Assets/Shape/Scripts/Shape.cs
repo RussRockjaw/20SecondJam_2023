ded292b222da32efd27e06c20585a2fc70d58316
Russ <Pokerchips90@gmail.com>
2023-11-07 20:22:27 -0500

    Adjusted spawned pieces pivot points

--- Assets/Shape/Scripts/Shape.cs
+++ Assets/Shape/Scripts/Shape.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Shape
 {
-    private Vector2[] nodePositions;
+    public Vector2[] nodePositions;
+    private Vector3 originPoint;
+    public float pivotOffset = 0.5f;
     
-    public Shape(Vector2[] nodePositions)
+    //Supply node positions in local xy coordinates relative to the step algorithm start point.
+    //ie startPos + (1, 0) is right one cell
+    //startPosition is in world space where you want to start drawing the piece
+    public Shape(Vector2[] nodePositions, Vector3 startPosition)
     {
         this.nodePositions = nodePositions;
+        originPoint = new Vector3(startPosition.x + pivotOffset, startPosition.y + pivotOffset);
         CreateMesh();
     }
     
     private void CreateMesh()
     {
-        Vector3 position = new Vector3(0, 0, 0);
+        //Create GameObjects and use parent as pivot
         GameObject obj = new GameObject();
+        GameObject parent = new GameObject();
+        parent.transform.position = originPoint;
+        obj.transform.SetParent(parent.transform);
+        
+        //Add components to GameObjects
         MeshFilter meshFilter = obj.AddComponent(typeof(MeshFilter)) as MeshFilter;
         MeshRenderer meshRenderer = obj.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
         meshRenderer.material = new Material(Shader.Find("Sprites/Default"));
         meshRenderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), 1);
-
+        
+        //Data for mesh generation
         Vector3[] verts = new Vector3[nodePositions.Length * 4];
         Vector2[] uvs = new Vector2[nodePositions.Length * 4];
         int[] tris = new int[nodePositions.Length * 2 * 3];
@@ -29,17 +41,17 @@ public class Shape
 
         for(int i = 0; i < verts.Length; i += 4)
         {
-            //Set quad's vertices
-            verts[i] = new Vector3(0 + nodePositions[nodeCount].x, 0 + nodePositions[nodeCount].y);
-            verts[i + 1] = new Vector3(0 + nodePositions[nodeCount].x, 1 + nodePositions[nodeCount].y);
-            verts[i + 2] =  new Vector3(1 + nodePositions[nodeCount].x, 1 + nodePositions[nodeCount].y);
-            verts[i + 3] = new Vector3(1 + nodePositions[nodeCount].x, 0 + nodePositions[nodeCount].y);
+            //Set quad's vertices. Subtract the offset to fit to grid visually.
+            verts[i] = new Vector3(originPoint.x - pivotOffset + nodePositions[nodeCount].x, originPoint.y - pivotOffset + nodePositions[nodeCount].y);
+            verts[i + 1] = new Vector3(originPoint.x - pivotOffset + nodePositions[nodeCount].x, originPoint.y - pivotOffset + 1 + nodePositions[nodeCount].y);
+            verts[i + 2] = new Vector3(originPoint.x - pivotOffset + 1 + nodePositions[nodeCount].x, originPoint.y - pivotOffset + 1 + nodePositions[nodeCount].y);
+            verts[i + 3] = new Vector3(originPoint.x - pivotOffset + 1 + nodePositions[nodeCount].x, originPoint.y - pivotOffset + nodePositions[nodeCount].y);
             
             //Make new UVs to match new vertices
-            uvs[i] = new Vector2(0 + nodePositions[nodeCount].x, 0 + nodePositions[nodeCount].y);
-            uvs[i + 1] = new Vector2(0 + nodePositions[nodeCount].x, 1 + nodePositions[nodeCount].y);
-            uvs[i + 2] =  new Vector2(1 + nodePositions[nodeCount].x, 1 + nodePositions[nodeCount].y);
-            uvs[i + 3] = new Vector2(1 + nodePositions[nodeCount].x, 0 + nodePositions[nodeCount].y);
+            uvs[i] = new Vector3(originPoint.x - pivotOffset + nodePositions[nodeCount].x, originPoint.y - pivotOffset + nodePositions[nodeCount].y);
+            uvs[i + 1] = new Vector3(originPoint.x - pivotOffset + nodePositions[nodeCount].x, originPoint.y - pivotOffset + 1 + nodePositions[nodeCount].y);
+            uvs[i + 2] =  new Vector3(originPoint.x - pivotOffset + 1 + nodePositions[nodeCount].x, originPoint.y - pivotOffset + 1 + nodePositions[nodeCount].y);
+            uvs[i + 3] = new Vector3(originPoint.x - pivotOffset + 1 + nodePositions[nodeCount].x, originPoint.y - pivotOffset + nodePositions[nodeCount].y);
             
             //Create triangle 1 of new quad
             tris[triCount] = i;
@@ -54,13 +66,12 @@ public class Shape
             triCount += 6;
             nodeCount++;
         }
-
+        
         Mesh mesh = new Mesh();
         mesh.vertices = verts;
         mesh.uv = uvs;
         mesh.triangles = tris;
         mesh.name = "Piece";
         meshFilter.mesh = mesh;
-
     }
 }
--- Assets/Shape/Scripts/ShapeGenerator.cs
+++ Assets/Shape/Scripts/ShapeGenerator.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class ShapeGenerator : MonoBehaviour
 {
-    public Vector2[] nodePositions;
+    public Vector2[] piece1;
+    public Vector2[] piece2;
+    public Vector2[] piece3;
+    public Vector2[] piece4;
+    public Vector2[] piece5;
+    
+    public Vector3[] originPoints;
+
     // Start is called before the first frame update
     void Start()
     {
-        Shape shape = new Shape(nodePositions);
+        Shape shape1 = new Shape(piece1, originPoints[0]);
+        Shape shape2 = new Shape(piece2, originPoints[1]);
+        Shape shape3 = new Shape(piece3, originPoints[2]);
+        Shape shape4 = new Shape(piece4, originPoints[3]);
+        Shape shape5 = new Shape(piece5, originPoints[4]);
     }
 
     // Update is called once per frame
