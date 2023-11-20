2d87f686ddf98fdc922b33484861bacf4c04d00b
Russ <Pokerchips90@gmail.com>
2023-11-19 21:38:51 -0500

    Finished implementing option sliders

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -1,26 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
-    public GameObject returnButton;
-    public GameObject timerSlider;
-    public GameObject timerValue;
-    public GameObject widthSlider;
-    public GameObject widthValue;
-    public GameObject heightSlider;
-    public GameObject heightValue;
+    public GameObject returnButtonGO;
+
+    public GameObject timerSliderGO;
+    private Slider timerSlider;
+    public GameObject labelTimerValue;
+    public int TimerValue {get;}
+    
+    public GameObject widthSliderGO;
+    private Slider widthSlider;
+    public GameObject labelWidthValue;
+    private int WidthValue {get;}
+    
+    public GameObject heightSliderGO;
+    private Slider heightSlider;
+    public GameObject labelHeightValue;
+    private int HeightValue {get;}
 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void Setup()
+    {
+       timerSlider = timerSliderGO.GetComponent<Slider>();
+       timerSlider.onValueChanged.AddListener(delegate {TimerSliderChange();});
+       widthSlider = widthSliderGO.GetComponent<Slider>();
+       widthSlider.onValueChanged.AddListener(delegate {WidthSliderChange();});
+       heightSlider = heightSliderGO.GetComponent<Slider>();
+       heightSlider.onValueChanged.AddListener(delegate {HeightSliderChange();});
+    }
+
+    public void TimerSliderChange()
+    {
+        labelTimerValue.GetComponent<TMP_Text>().text = "" + timerSlider.value;
+    }
+
+    public void WidthSliderChange()
+    {
+        labelWidthValue.GetComponent<TMP_Text>().text = "" + widthSlider.value;
+    }
+
+    public void HeightSliderChange()
+    {
+        labelHeightValue.GetComponent<TMP_Text>().text = "" + heightSlider.value;
     }
 }
